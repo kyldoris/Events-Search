7cd74ec1942e35ad7863eaa76c6072f0c296a9ae
Kwok yan Lau <dorislau@kwoks-mbp.lan>
2022-08-25 17:42:15 -0700

    final

--- script.js
+++ script.js
@@ -1,69 +1,57 @@
 
-// // WAYS TO request data via our API and JS
-// // 1. jQuery's $.ajax()
-// // 2. tje browser fetch
-// //3. JS HTTP client called Axios - installed 
-// console.log('connected')
-// console.log($.ajax('https://www.omdbapi.com/?apikey=83ec9128=titan'))
-
-
-////////////////////////////////////////////////////////////////////////
-// VARIABLES
-// ////////////////////////////////////////////////////////////////////////
-// const URL = "https://app.ticketmaster.com/discovery/v2/events.json?size=50&apikey=GWOqtM28DNuPgxuINJAehHv60umwRwjr";
-
-
-// ////////////////////////////////////////////////////////////////////////
-// // CACHED ELEMENTS/ ELEMENTS REFRENCES
-// ////////////////////////////////////////////////////////////////////////
-
-// // const $title = $('#title')
-// // const $year = $('#year')
-// // const $rated = $('#rated')
-// const $form = $('form')
-// const $input = $('input[type="text"]') //< js know specifically which input to look for 
-
-// ////////////////////////////////////////////////////////////////////////
-// //EVENT LISTENERS
-// ////////////////////////////////////////////////////////////////////////
-// $form.on('submit', handleGetData)
 
 
 
-////////////////////////////////////////////////////////////////////////
-// FUNCTIONS
-////////////////////////////////////////////////////////////////////////
-// function handleGetData(event) {
-//     event.preventDefault() //< to stop broswer from refreshing every time we click 
-//     userInput = $input.val ()
-//     if (userInput === '') return;
+const $form = $('form')
+const $input = $('input[type="text"]')
+// const $input = document.getElementById('searchBar').value //< js know specifically which input to look for
+// const UseInput = "EDM" 
 
-// $.ajax(URL+userInput).then(
-//     function (json) {
-//         console.log("movie is ready!");
-//         console.log(json);
-//         // $title.text(data.Title)
-//         // $year.text(data.Year)
-//         // $rated.text(data.Rated)
-//         // $('main').append(`<img src="${data.Poster}" alt="${data.Title}"/>`)
-//         // $input.val('')
-//     },
-//     function (error) {
-//         console.log("we broke it!")
-//         console.log(error);
-//     }
-// // );
-$.ajax({
-    type:"GET",
-    url:"https://app.ticketmaster.com/discovery/v2/events.json?size=100&apikey=GWOqtM28DNuPgxuINJAehHv60umwRwjr",
-    async:true,
-    dataType: "json",
-    success: function(json) {
-                console.log(json);
-                // Parse the response.
-                // Do other things.
+
+$form.on('submit', handleGetData)
+
+
+function handleGetData(event) {
+   event.preventDefault() //< to stop broswer from refreshing every time we click 
+    userInput = $input.val();
+   if (userInput === '') return;
+
+const URL = "https://app.ticketmaster.com/discovery/v2/events.json?keyword="+userInput+"&countryCode=US&size=20&apikey=GWOqtM28DNuPgxuINJAehHv60umwRwjr"
+
+$.ajax(URL).then(
+
+    function(json) {
+      
+               $('.information').html("<h2><b>EVENTS & DETAILS</b></h2><hr>")
+
+                const eventInfoList = [];
+                const eventInfo =  json._embedded.events;
+                for (let i = 0; i < eventInfo.length; i++) {
+                  const eventInfoList = eventInfo[i];
+                     console.log(eventInfoList)
+
+               $('.information').append(
+                  "<h3>" + eventInfo[i].name + "</h3>" +
+                  "<p><img src='" + eventInfo[i].images[5].url + "' width='305'></p>"+
+                  "<p>Event's Date: " + eventInfo[i].dates.start.localDate + "</p>"+
+                  "<p>Sales Date: " + eventInfo[i].sales.public.startDateTime + "</p>"+
+                  "<p>Event's ID: " + eventInfo[i].id + "</p>"+
+                  "<p>For more info: <a href ='" + eventInfo[i].url + "'>" + eventInfo[i].url + "</a></p>"+
+               
+                  "<hr>"
+               );
+                }
+   
              },
-    error: function(xhr, status, err) {
-                // This time, we do not end up here!
-             }
-        });
+    function(xhr, status, err) {
+        console.log('Sorry! We broke it! Check back later!')
+               // This time, we do not end up here!
+            }
+  
+        // }
+           ); 
+        
+};
+// handleGetData("edm");
+
+
